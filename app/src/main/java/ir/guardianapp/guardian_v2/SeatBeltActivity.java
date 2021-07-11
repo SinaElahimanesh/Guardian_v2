96e384ef43df166771a89907f238f1c7823f57c2
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2021-07-11 15:25:41 +0430

    add SeatBeltActivity.java (SEAT_BELT PAGE) + designing it

---
+++ app/src/main/java/ir/guardianapp/guardian_v2/SeatBeltActivity.java
@@ -0,0 +1,14 @@
+package ir.guardianapp.guardian_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeatBeltActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_belt);
+    }
+}
