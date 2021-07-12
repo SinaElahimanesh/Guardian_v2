6880f78bd3a6834ba70eda6bc2aaf7b705da08cc
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2021-07-12 12:49:48 +0430

    add SleepManagerActivity.java (SLEEP_MANAGER PAGE) + designing it + some bugs about Login/Register Pages

---
+++ app/src/main/java/ir/guardianapp/guardian_v2/SleepManagerActivity.java
@@ -0,0 +1,21 @@
+package ir.guardianapp.guardian_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SleepManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_manager);
+    }
+
+    public void submit(View view) {
+    }
+
+    public void recordAuto(View view) {
+    }
+}
