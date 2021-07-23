b0a46fa2cf0803d0f20526ff7e53df7a1ed43632
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2021-07-23 14:03:23 +0430

    edit something

--- app/src/main/java/ir/guardianapp/guardian_v2/SleepSpeedManager/UseMeNotification.java
+++ app/src/main/java/ir/guardianapp/guardian_v2/SleepSpeedManager/UseMeNotification.java
@@ -13,7 +13,6 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import com.google.gson.Gson;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
